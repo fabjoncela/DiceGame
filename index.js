60ce0a9179a1c28881b6a730349157563e79a94b
Fabjon Cela <99296979+fabjoncela@users.noreply.github.com>
2024-07-25 16:48:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function rollDice() {
     if (randomNumber1 > randomNumber2) {
       document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
     } else if (randomNumber2 > randomNumber1) {
-      document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+      document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
     } else {
       document.querySelector("h1").innerHTML = "Draw!📍";
     }
